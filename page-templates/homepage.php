476c24d6a4f10920a19c5422842a87f013578496
mxp83 <marc.payumo@gmail.com>
2016-10-12 07:43:23 -0700

    Added initial styles/logo

---
+++ page-templates/homepage.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Template Name: Homepage
+*/
+get_header(); ?>
+
+<header id="front-h" role="banner">
+	<?php putRevSlider("frontHero") ?>
+</header>
+<div class="full-width-bg grey">
+	<div class="row" id="intro-box">
+		<div class="small-12 medium-4 large-4  medium-offset-8 large-offset-8 columns">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam eius quos officiis sint rerum voluptatum eligendi dicta deleniti totam molestias. Ipsa blanditiis illo neque tenetur eius inventore officiis nesciunt aspernatur repudiandae laudantium hic vel quidem at maiores, iusto. Deleniti, quisquam.
+		</div>
+	</div>
+</div>
+
